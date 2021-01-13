46987589b89ebcea4dd385cf51aaeddee2d15b89
Iulia R Ionita <iulia@ionita.me>
2021-01-13 16:53:49 +0000

    Endpoint fully working with simple token based auth
    Added the manifest.yml file
    
    Added the public folder uploads to gitignore
    
    Cleanup test files after running specs
    
    Added the cifignore file
    
    Removed redundant message
    
    Removed duplicate command from rails_helper
    
    Added carrierwave initializer
    
    Fixed typo in Readme

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,32 @@
+include ActionController::HttpAuthentication::Basic::ControllerMethods
+include ActionController::HttpAuthentication::Token::ControllerMethods
+
+class DocumentsController < ApplicationController
+  before_action :authenticate
+
+  def show
+    document = Document.find_by_id(document_params[:id])
+
+    if document.nil?
+      render status: :not_found
+    elsif document && !document.document_file.file && %w[safe unsafe].any?{|state| state.include?(document.state.to_s) }
+      render json: document.to_json, status: :gone
+    else
+      render json: document.to_json, status: :ok
+    end
+  end
+
+  private
+
+  def document_params
+    params.permit(:id)
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |source_app, api_key|
+      @client = Client.find_by_source_app(source_app)
+      @client && @client.api_key == api_key
+    end
+  end
+
+end
---
+++ app/uploaders/document_file_uploader.rb
@@ -0,0 +1,28 @@
+class DocumentFileUploader < CarrierWave::Uploader::Base
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage Rails.env.development? || Rails.env.test? ? :file : :aws
+
+  def size_range
+    1.byte..5.gigabytes
+  end
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    if Rails.env.test?
+      "uploads/test/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    else
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_whitelist
+    %w(pdf csv docx xlsx)
+  end
+end
