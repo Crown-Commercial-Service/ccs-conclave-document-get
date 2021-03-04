62ccc1d3651693b693242eddff1c8c1480895970
Reem Ibrahim <reem.ibrahim@co-cc02839.ad.cabinetoffice.gov.uk>
2021-03-04 10:58:42 +0000

    removed unneeded code

--- app/uploaders/document_file_uploader.rb
+++ app/uploaders/document_file_uploader.rb
@@ -6,10 +6,6 @@ class DocumentFileUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage Rails.env.production? ? :aws : :file
 
-  def size_range
-    1.byte..5.gigabytes
-  end
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -20,12 +16,6 @@ class DocumentFileUploader < CarrierWave::Uploader::Base
     else
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
-  end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_whitelist
-    %w[pdf csv docx xlsx]
   end
 
   def as_json(_options = nil)
